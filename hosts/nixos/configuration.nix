53637237fe79e95d46fb322c16aa768f48adb151
Vicyde <jreuhman@fastmail.nl>
2025-03-29 21:33:44 +0100

    Removed gnome as desktop environment

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -39,7 +39,7 @@
 
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  # services.xserver.desktopManager.gnome.enable = true;
 
   programs.ssh.askPassword = pkgs.lib.mkForce "${pkgs.seahorse.out}/libexec/seahorse/ssh-askpass";
 
