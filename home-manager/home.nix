346b2c05fbdabc1ccee59bacf548bd6e63ab8ebb
Vicyde <jreuhman@fastmail.nl>
2025-04-09 18:03:24 +0200

    Added emacs configuration

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -10,7 +10,6 @@
 
   home.packages = with pkgs; [
     brave
-    emacs
     libreoffice-qt-fresh
     obsidian
     vscode
--- home-manager/modules/emacs.nix
+++ home-manager/modules/emacs.nix
@@ -1,10 +1,61 @@
+{pkgs, ...}:
 {
   services.emacs = {
     enable = true;
-    package = pkgs.emacs;
+    package = with pkgs; (
+        (emacsPackagesFor pkgs.emacs).emacsWithPackages (
+            epkgs: [
+                epkgs.dashboard
+                epkgs.evil
+                epkgs.general
+                epkgs.ivy
+                epkgs.magit
+                epkgs.which-key
+                epkgs.doom-themes
+            ]
+        )
+    );
   };
 
   programs.emacs = {
     enable = true;
+
+    extraPackages = epkgs: with epkgs; [
+        epkgs.dashboard
+        epkgs.evil
+        epkgs.general
+        epkgs.ivy
+        epkgs.magit
+        epkgs.which-key
+        epkgs.doom-themes
+    ];
+
+    extraConfig = ''
+        (dashboard-setup-startup-hook)
+
+        (evil-mode)
+
+        (which-key-mode 1)
+
+        (setq doom-themes-enable-bold t
+              doom-themes-enable-italic t)
+        (load-theme 'doom-one t)
+
+        (setq inhibit-startup-screen t)
+
+        (menu-bar-mode -1)
+        (tool-bar-mode -1)
+        (scroll-bar-mode -1)
+
+        (global-display-line-numbers-mode 1)
+        (setq display-line-numbers-type 'relative)
+
+        (global-visual-line-mode t)    
+
+        (set-face-attribute 'default nil
+            :font "SpaceMono Nerd Font"
+            :height 150
+            :weight 'normal) 
+    '';
   };
 }
